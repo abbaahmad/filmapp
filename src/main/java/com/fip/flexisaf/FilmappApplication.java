d5df8fb1d55fb34c97bf203454db214b2fdee0b8
abbaahmad <abbaahmad50@yahoo.co.uk>
2022-05-27 14:52:26 +0100

    Project initialisation

---
+++ src/main/java/com/fip/flexisaf/FilmappApplication.java
@@ -0,0 +1,13 @@
+package com.fip.flexisaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fip/flexisaf/FilmappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fip.flexisaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
